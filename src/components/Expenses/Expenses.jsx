0bb1bafcc401d525e16ba51a7c6a7432510c06e4
Solo Abramov <59119335+soloAbramov@users.noreply.github.com>
2021-09-13 10:53:44 +0300

    Add files via upload

---
+++ src/components/Expenses/Expenses.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2021");
+
+  const filterChange = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChange} />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return <h1 className="header">Expenses Tracker</h1>;
+};
+
+export default Header;
